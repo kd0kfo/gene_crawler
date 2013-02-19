d0a09efcf372a3dced0f819ebe6a29245af7eb57
David Coss <David.Coss@stjude.org>
2013-02-19 16:02:44 -0600

    Added setup files.

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup, Extension
+
+the_scripts = ['scripts/gene_extractor','scripts/regex_search']
+
+setup (name ='gene_crawler',
+       version = '0.3',
+       url = 'http://code.davecoss.com',
+       license = 'GPL v3',
+       description = 'Utility functions for searching and manipulating gene data',
+       author_email='David.Coss@stjude.org',
+       packages = ['gene_crawler'],
+       scripts = the_scripts,
+       )
+
